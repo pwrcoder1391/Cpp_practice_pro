951ea36d1614b6e6f20e019ffc5020a9a429a691
pwrcoder1391 <pro1391engg@gmail.com>
2023-06-09 20:34:21 +0530

    Create Helloworld.c
    Just a initial checkin

---
+++ Helloworld.c
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std ; 
+
+int main ()
+{
+  int a = 0 ; 
+  
+  if (a == 0)
+  {
+    cout << "Hello damn world" << endl;
+  }
+  else 
+  {
+    cout << "guess i am non-existant" << endl ; 
+  }
+  
+  return 0 ;
+}
