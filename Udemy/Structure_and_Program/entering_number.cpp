801a0c8cfa60cbf91904227c2c0fbb9f4a664856
pwrcoder1391 <pro1391engg@gmail.com>
2023-06-09 21:42:57 +0530

    Create entering_number.cpp

---
+++ Udemy/Structure_and_Program/entering_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std ; 
+
+int main()
+{
+
+    int a ;
+    int b ;
+
+    cout << "1st number" ;
+    cin >> a ;
+    cout << "2nd number" ;
+    cin >> b ;
+
+    // if you put space between the integer , its gonna show the same thing 
+
+    cout <<"1st number" << a <<"2nd number"<< b  ; 
+
+    return 0 ;
+}
