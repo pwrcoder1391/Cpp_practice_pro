c60c3645f0c8f0e2f074fa250b87cc8d30dde447
pwrcoder1391 <pro1391engg@gmail.com>
2023-08-07 23:20:15 +0530

    constructors

---
+++ Udemy/member_objects/copy_constructor/move_constructor.cpp
@@ -0,0 +1,106 @@
+/*
+copy constructor is resource intensive 
+
+    when we assign something --> x=30 , x{30} --> these times 30 is 1st stored in a temp variable and then in x
+
+    so when we use copy constructor (eg) --> class class1 = class2 {initializing class1 by class2} 
+    class2 is 1st assigned to a temporary memory and then to class1 
+
+    This happens when we use Copy constructor 
+
+When we use Move Constructor --
+
+    We do copy similar to "Member-wise copy" i.e {data = source.data} 
+    but after we do "member-wise copy" we assign the pointer/memory to a "nullptr" --> hence freeing/allocating blank memory 
+
+
+
+*/
+
+
+#include<iostream>
+
+using namespace std;
+
+class move_copy
+{
+
+private:
+
+int*data;
+
+public:
+
+/*here we'll create 
+
+- parametrized constructor 
+- copy constructor 
+- move constructor 
+- destructor 
+- function to show the value 
+
+*/
+
+move_copy(int a) //parametrized constructor
+{
+    *data =  a;
+    cout<<"parametrized constructor"<<endl;
+}
+
+
+move_copy(const move_copy&source) //copy constructor ** here we can use "const" as well 
+{
+    data = new int; // new memory assigned
+    *data = *(source.data); // the contained (where the pointer points) is copied
+    cout<<"deep copy constructor is called"<<endl;
+}
+
+move_copy(move_copy&& source) // move constructor
+{
+    data = source.data;
+    data = nullptr;
+    cout<<"move copy complete"<<endl;
+}
+
+~move_copy()
+{
+    if(data!=nullptr)
+    {
+        cout<<"deleting"<<(*data)<<endl;
+    }
+    else 
+    {
+        cout<<"deleting nullptr"<<endl;
+    }
+
+    delete data;
+}
+
+void show_move()
+{
+    cout<<(*data)<<endl;    
+}
+
+
+};
+
+
+void call(move_copy x) // here instead of calling the copy pointer the move pointer will be called
+{
+    x.show_move();
+
+}
+
+
+int main()
+{
+    move_copy x(55);
+
+    move_copy z(x); 
+
+    call(z); // should call the move constructor    
+
+    /* here i was trying to cout<<call(z); --> which wont work since there is nothing to cout*/
+
+    return 0 ;
+}
