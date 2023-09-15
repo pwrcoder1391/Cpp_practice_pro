88ba97edd8a02728e2a9d7caaaf716ff3e459f35
pwrcoder1391 <pro1391engg@gmail.com>
2023-09-16 01:50:18 +0530

    2nd checkin 16/09/23

---
+++ Udemy/Polymorphism/Challenge/Animal.h
@@ -0,0 +1,18 @@
+/*****************************/
+/** DO NOT MODIFY THIS FILE **/
+/*****************************/
+
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <string>
+
+// Abstract base class Animal
+
+class Animal {
+public:
+    virtual std::string get_noise() = 0; // Pure virtual function
+    virtual int get_num_legs() = 0; // Pure virtual function
+};
+
+#endif /* ANIMAL_H */
